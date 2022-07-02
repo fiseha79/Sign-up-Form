1b9caea1305d1e6356e56f7467f2f1caf9828fe3
Fiseha <famarkfish79@gmail.com>
2022-07-02 23:47:49 +0300

    Finall comit

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 function onChange() {
+    
+    const msg = document.querySelector('#error');
+    const content = document.createElement('h4');
     const password = document.querySelector('input[name=password]');
     const confirm = document.querySelector('input[name=confirm]');
     if (confirm.value === password.value) {
 
-       document.getElementsByClassName("error").innerHTML = "Password Match"; 
+       content.textContent = 'Password Valid';
+       msg.appendChild(content);
     } else {
-        document.getElementsByClassName("error").innerHTML = "Passwords do not match";
+        content.textContent = '*Password Does not Match';
+
+       
+        msg.appendChild(content);
+        msg.style.color = 'red';
+        
     }
+    
+    
+    
+
   }
+
+ 
+  const btn = document.querySelector('#btn');
+  btn.addEventListener('click', onChange);
